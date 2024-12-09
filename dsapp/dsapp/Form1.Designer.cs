cf8108e969e359b0c3b5f0f690ba5803da410fdc
vinx342 <vgosorio8@gmail.com>
2024-12-09 18:49:33 +0800

    Add files via upload

---
+++ dsapp/dsapp/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace dsapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idTBox = new System.Windows.Forms.TextBox();
+            this.FNameTBox = new System.Windows.Forms.TextBox();
+            this.LNameTBox = new System.Windows.Forms.TextBox();
+            this.AgeTBox = new System.Windows.Forms.TextBox();
+            this.adrsTBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.RegisButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // idTBox
+            // 
+            this.idTBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idTBox.Location = new System.Drawing.Point(140, 109);
+            this.idTBox.Name = "idTBox";
+            this.idTBox.Size = new System.Drawing.Size(238, 30);
+            this.idTBox.TabIndex = 0;
+            // 
+            // FNameTBox
+            // 
+            this.FNameTBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FNameTBox.Location = new System.Drawing.Point(141, 165);
+            this.FNameTBox.Name = "FNameTBox";
+            this.FNameTBox.Size = new System.Drawing.Size(194, 30);
+            this.FNameTBox.TabIndex = 1;
+            // 
+            // LNameTBox
+            // 
+            this.LNameTBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LNameTBox.Location = new System.Drawing.Point(141, 218);
+            this.LNameTBox.Name = "LNameTBox";
+            this.LNameTBox.Size = new System.Drawing.Size(194, 30);
+            this.LNameTBox.TabIndex = 2;
+            // 
+            // AgeTBox
+            // 
+            this.AgeTBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AgeTBox.Location = new System.Drawing.Point(466, 112);
+            this.AgeTBox.Name = "AgeTBox";
+            this.AgeTBox.Size = new System.Drawing.Size(98, 30);
+            this.AgeTBox.TabIndex = 3;
+            // 
+            // adrsTBox
+            // 
+            this.adrsTBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adrsTBox.Location = new System.Drawing.Point(503, 168);
+            this.adrsTBox.Name = "adrsTBox";
+            this.adrsTBox.Size = new System.Drawing.Size(308, 30);
+            this.adrsTBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ID Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "First Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Last Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(406, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Age:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(406, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Address:";
+            // 
+            // RegisButton
+            // 
+            this.RegisButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisButton.Location = new System.Drawing.Point(587, 242);
+            this.RegisButton.Name = "RegisButton";
+            this.RegisButton.Size = new System.Drawing.Size(137, 58);
+            this.RegisButton.TabIndex = 5;
+            this.RegisButton.Text = "Register";
+            this.RegisButton.UseVisualStyleBackColor = true;
+            this.RegisButton.Click += new System.EventHandler(this.RegisButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 323);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(829, 219);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeButton.Location = new System.Drawing.Point(730, 242);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(95, 58);
+            this.removeButton.TabIndex = 7;
+            this.removeButton.Text = "Delete";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(416, 52);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Student Registration";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 559);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.RegisButton);
+            this.Controls.Add(this.adrsTBox);
+            this.Controls.Add(this.AgeTBox);
+            this.Controls.Add(this.LNameTBox);
+            this.Controls.Add(this.FNameTBox);
+            this.Controls.Add(this.idTBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox idTBox;
+        private System.Windows.Forms.TextBox FNameTBox;
+        private System.Windows.Forms.TextBox LNameTBox;
+        private System.Windows.Forms.TextBox AgeTBox;
+        private System.Windows.Forms.TextBox adrsTBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button RegisButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ dsapp/dsapp/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dsapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = null;
+            SqlConnection cnn;
+            connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\vinx\\source\\repos\\dsapp\\dsapp\\MyDB1.mdf;Integrated Security=True";
+            cnn = new SqlConnection(connectionString);
+
+            cnn.Open();
+
+            int IDTEST = Convert.ToInt32(idTBox.Text);
+            int AGETEST = Convert.ToInt32(AgeTBox.Text);
+
+            SqlCommand sqlcmd = new SqlCommand("INSERT into tbl_Student values ("+IDTEST+",'"+FNameTBox.Text+"','"+LNameTBox.Text+"','" +AGETEST+ "','"+adrsTBox.Text+"')", cnn);
+            sqlcmd.ExecuteNonQuery();
+            cnn.Close();
+
+            refresh();
+        }
+
+        public void refresh()
+        {
+            string connectionString = null;
+            SqlConnection cnn;
+            connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\vinx\\source\\repos\\dsapp\\dsapp\\MyDB1.mdf;Integrated Security=True";
+            cnn = new SqlConnection(connectionString);
+
+            cnn.Open();
+            SqlDataAdapter sqlDA = new SqlDataAdapter("SELECT * from tbl_Student", cnn);
+            DataTable dt = new DataTable();
+            sqlDA.Fill(dt);
+            dataGridView1.DataSource = dt;
+            cnn.Close();
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = null;
+            SqlConnection cnn;
+            connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\vinx\\source\\repos\\dsapp\\dsapp\\MyDB1.mdf;Integrated Security=True";
+            cnn = new SqlConnection(connectionString);
+
+            cnn.Open();
+
+            int IDTEST = Convert.ToInt32(idTBox.Text);
+            int AGETEST = Convert.ToInt32(AgeTBox.Text);
+
+            SqlCommand sqlcmd = new SqlCommand("DELETE from tbl_Student", cnn);
+            sqlcmd.ExecuteNonQuery();
+            cnn.Close();
+
+            refresh();
+        }
+    }
+}
